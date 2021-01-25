ef6c9f3e3e88776ea703eb70ace8c6c87c8c054a
Shubhangi Bhargava <54638807+shubhangi2803@users.noreply.github.com>
2021-01-25 23:52:44 +0530

    Update settings.py

--- MyHomeFinder/settings.py
+++ MyHomeFinder/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'MyHomeFInder/static')
+    os.path.join(BASE_DIR, 'MyHomeFinder/static')
 ]
 
 # Media Folder Settings
